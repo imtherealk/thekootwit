a6357adf541a80152375804b55e2c74dce7c2034
imtherealk <imtherealk@gmail.com>
2015-09-05 15:29:03 +0900

    CustomTwitterApiClient, UserService 추가

---
+++ app/src/main/java/com/realk/thekootwit/CustomTwitterApiClient.java
@@ -0,0 +1,37 @@
+package com.realk.thekootwit;
+
+import com.realk.thekootwit.service.UserService;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Session;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Custom twitter client
+ */
+public class CustomTwitterApiClient extends TwitterApiClient {
+    private static Map<Session, CustomTwitterApiClient> sharedClients = new HashMap<>();
+
+    public CustomTwitterApiClient(Session session) {
+        super(session);
+    }
+
+    public UserService getUserService() {
+        return this.getService(UserService.class);
+    }
+
+    public static CustomTwitterApiClient getSharedClient(Session session) {
+        if (!sharedClients.containsKey(session)) {
+            sharedClients.put(session, new CustomTwitterApiClient(session));
+        }
+        return sharedClients.get(session);
+    }
+
+    public static CustomTwitterApiClient getActiveClient() {
+        TwitterSession session = Twitter.getSessionManager().getActiveSession();
+        return getSharedClient(session);
+    }
+}
---
+++ app/src/main/java/com/realk/thekootwit/service/UserService.java
@@ -0,0 +1,17 @@
+package com.realk.thekootwit.service;
+
+import com.twitter.sdk.android.core.models.User;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+/**
+ * Retrofit twitter service
+ */
+public interface UserService {
+    @GET("/users/search.json")
+    void search(@Query("q") String query, @Query("page") int page, Callback<List<User>> callback);
+}
