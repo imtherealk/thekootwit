bd18bcf75a0b74ab4937768cd07ec4447e9e29dd
imtherealk <imtherealk@gmail.com>
2015-09-18 19:12:57 +0900

    Search 구현

--- app/src/main/java/com/realk/thekootwit/activity/SearchActivity.java
+++ app/src/main/java/com/realk/thekootwit/activity/SearchActivity.java
@@ -12,16 +12,18 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.realk.thekootwit.CustomTwitterApiClient;
 import com.realk.thekootwit.R;
+import com.squareup.picasso.Picasso;
 import com.twitter.sdk.android.core.models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.Callback;
@@ -30,23 +32,20 @@ import retrofit.client.Response;
 
 
 public class SearchActivity extends Activity {
-    CustomTwitterApiClient client;
     private String query = null;
     private int page = 1;
     EditText searchEditText;
     Button searchButton;
     ListView searchResultListView;
-    List<User> users = null;
+    List<User> users = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        client = CustomTwitterApiClient.getActiveClient();
         searchEditText = (EditText) findViewById(R.id.searchbox);
         searchButton = (Button) findViewById(R.id.btnsearch);
         searchResultListView = (ListView) findViewById(R.id.searchresult);
-
 
         searchResultListView.setAdapter(searchResultAdapter);
 
@@ -59,61 +58,7 @@ public class SearchActivity extends Activity {
         });
 
     }
-
-    searchResultListAdapter searchResultAdapter = new searchResultListAdapter(this, R.layout.listview_item_search, users);
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    void updateUserList(List<User> users) {
-        this.users = users;
-    }
-
-    void searchUsersFromTwitter() {
-        client.getUserService().search(query, page, new Callback<List<User>>() {
-            @Override
-            public void success(List<User> users, Response response) {
-                updateUserList(users);
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Toast.makeText(SearchActivity.this, error.getLocalizedMessage(), Toast.LENGTH_LONG);
-            }
-        });
-    }
-
-    class searchResultListAdapter extends BaseAdapter {
-        LayoutInflater inflater;
-        Context context;
-        int layout;
-        List<User> users;
-        public searchResultListAdapter(Context context, int layout, List<User> users) {
-            this.context = context;
-            this.layout = layout;
-            this.users = users;
-            inflater = LayoutInflater.from(context);
-        }
-
+    private final BaseAdapter searchResultAdapter = new BaseAdapter() {
         @Override
         public int getCount() {
             return users.size();
@@ -141,14 +86,14 @@ public class SearchActivity extends Activity {
                 viewHolder.profleImage = (ImageView) convertView.findViewById(R.id.user_profile_image);
                 viewHolder.username = (TextView) convertView.findViewById(R.id.username);
                 viewHolder.biography = (TextView) convertView.findViewById(R.id.biography);
-                viewHolder.followButton = (Button) convertView.findViewById(R.id.btnfollow);
+                viewHolder.followButton = (ImageButton) convertView.findViewById(R.id.btnfollow);
                 convertView.setTag(viewHolder);
             } else {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
 
             final User user = (User) this.getItem(position);
-            viewHolder.profleImage.setImageURI(Uri.parse(user.profileImageUrl));
+            Picasso.with(SearchActivity.this).load(user.profileImageUrl).into(viewHolder.profleImage);
             viewHolder.username.setText(user.name);
             viewHolder.biography.setText(user.description);
 
@@ -173,11 +118,53 @@ public class SearchActivity extends Activity {
             });
             return convertView;
         }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    void updateUserList(List<User> users) {
+        this.users = users;
+        searchResultAdapter.notifyDataSetChanged();
+    }
+
+    void searchUsersFromTwitter() {
+        CustomTwitterApiClient.getActiveClient().getUserService().search(query, page, new Callback<List<User>>() {
+            @Override
+            public void success(List<User> users, Response response) {
+                updateUserList(users);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(SearchActivity.this, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
     private static class ViewHolder {
         ImageView profleImage;
         TextView username;
         TextView biography;
-        Button followButton;
+        ImageButton followButton;
     }
 }
