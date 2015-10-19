9172e9da483a9d1b9cace0e3595301dbdf607719
imtherealk <imtherealk@gmail.com>
2015-10-19 16:52:11 +0900

    트윗을 가져오는 메소드들을 추가

--- app/src/main/java/com/realk/thekootwit/activity/TimelineActivity.java
+++ app/src/main/java/com/realk/thekootwit/activity/TimelineActivity.java
@@ -11,17 +11,83 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.realk.thekootwit.CustomTwitterApiClient;
+import com.realk.thekootwit.Globals;
 import com.realk.thekootwit.R;
 import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.core.models.User;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class TimelineActivity extends Activity {
     ListView timelineListView;
     List<Tweet> tweets = new ArrayList<Tweet>();
+
+    boolean loading = false;
+    boolean noMore = false;
+
+    void updateTweets(List<Tweet> tweets) {
+        this.tweets = tweets;
+        timelineListAdapter.notifyDataSetChanged();
+    }
+    void appendTweets(List<Tweet> tweets) {
+        updateTweets(Lists.newLinkedList(Iterables.concat(this.tweets, tweets)));
+    }
+
+    void fetchTweets() {
+        if (loading) {
+            return;
+        }
+        loading = true;
+
+        CustomTwitterApiClient.getActiveClient().getCustomListService().statuses(Globals.LIST_SLUG, CustomTwitterApiClient.getActiveUserId(), new Callback<List<Tweet>>() {
+            @Override
+            public void success(List<Tweet> tweets, Response response) {
+                updateTweets(tweets);
+                loading = false;
+                noMore = false;
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(TimelineActivity.this, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    void fetchMoreTweets() {
+        if (loading || noMore) {
+            return;
+        }
+        loading = true;
+
+        CustomTwitterApiClient.getActiveClient().getCustomListService()
+                .statuses(Globals.LIST_SLUG, CustomTwitterApiClient.getActiveUserId(),
+                        this.tweets.get(this.tweets.size() - 1).getId(),
+                        new Callback<List<Tweet>>() {
+            @Override
+            public void success(List<Tweet> tweets, Response response) {
+                appendTweets(TimelineActivity.this.tweets);
+                loading = false;
+                noMore = tweets.isEmpty();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(TimelineActivity.this, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
     final BaseAdapter timelineListAdapter = new BaseAdapter() {
         class ViewHolder {
@@ -77,6 +143,7 @@ public class TimelineActivity extends Activity {
         setContentView(R.layout.activity_timeline);
 
         timelineListView = (ListView) findViewById(R.id.timeline);
+        timelineListView.setAdapter(timelineListAdapter);
     }
 
 }
